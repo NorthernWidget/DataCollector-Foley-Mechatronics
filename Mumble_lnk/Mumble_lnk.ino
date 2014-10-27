122fd2d24d734c3c124ab1db21778d415f29844e
sindrio12 <sindrio12@e04762d8-fc4d-4a97-984f-be148a7113a7>
2014-10-27 13:34:47 +0000

    Cods
    git-svn-id: https://repository.cs.ru.is/svn/t-411-mech-2014/group/DataCollector@1072 e04762d8-fc4d-4a97-984f-be148a7113a7

---
+++ Mumble_lnk/Mumble_lnk.ino
@@ -0,0 +1,40 @@
+#include <SerialGSM.h>
+#include <SoftwareSerial.h>
+
+SerialGSM cell(50,51);
+String Message;
+
+int LedPin = 30;
+
+  
+boolean sendonce=true;
+void setup(){  
+  Serial.begin(9600);
+  cell.begin(9600);
+  cell.Verbose(true);
+  cell.Boot();
+  cell.DeleteAllSMS();
+  cell.FwdSMS2Serial();
+  Serial.println("Sindri");
+  pinMode(LedPin, OUTPUT);
+  digitalWrite(LedPin, LOW);
+ }
+
+
+void loop(){
+  if (cell.ReceiveSMS()){
+	 Serial.print("Sender: ");
+	 Serial.println(cell.Sender());
+	 Serial.print("message: ");
+          Message = cell.Message();
+	 Serial.println(Message);
+         cell.DeleteAllSMS();
+  }
+    if(Message == "L1"){
+      digitalWrite(LedPin, HIGH); 
+
+    }
+
+  Message = "";
+
+}
---
+++ ReseveSms.ino
@@ -0,0 +1,24 @@
+#include <SerialGSM.h>
+#include <SoftwareSerial.h>
+SerialGSM cell(50,51);
+
+boolean sendonce=true;
+void setup(){  
+  Serial.begin(9600);
+  cell.begin(9600);
+  cell.Verbose(true);
+  cell.Boot();
+  cell.DeleteAllSMS();
+  cell.FwdSMS2Serial();
+ }
+
+
+void loop(){
+  if (cell.ReceiveSMS()){
+	 Serial.print("Sender: ");
+	 Serial.println(cell.Sender());
+	 Serial.print("message: ");
+	 Serial.println(cell.Message());
+	 cell.DeleteAllSMS();
+  }
+}
