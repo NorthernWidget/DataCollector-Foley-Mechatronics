9c704746dc5e9a65016b2393f385f1825e9bd92e
sveinnm12 <sveinnm12@e04762d8-fc4d-4a97-984f-be148a7113a7>
2014-11-04 15:04:06 +0000

    Implemented StorageService.h
    git-svn-id: https://repository.cs.ru.is/svn/t-411-mech-2014/group/DataCollector@1231 e04762d8-fc4d-4a97-984f-be148a7113a7

--- Software/StorageService.h
+++ Software/StorageService.h
@@ -1,1 +1,16 @@
-DataStorageService.h
+#ifndef StorageService_h   
+#define StorageService_h
+
+#include <arduino.h>
+
+class StorageService
+{
+public:
+  // Empty virtual destructor for proper cleanup
+  virtual StorageService() {}
+  virtual int save(String data)=0; //Returns number of bytes saved
+  virtual String load()=0; //Returns the string in buffer 
+  virtual int erase()=0; //Erase the buffer
+  virtual bool isFreeSpcace(int dataSize) = 0; //Returns number of free bytes
+};
+#endif
